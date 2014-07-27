54b7f4791bf4f80d0f53080e8b571a772d307f94
pcanella <pcanella@ebay.com>
2014-07-26 23:14:31 -0700

    More updates

--- controllers/register/index.js
+++ controllers/register/index.js
@@ -2,23 +2,48 @@
 
 
 var RegisterModel = require('../../models/register');
+var model = new RegisterModel();
 
+module.exports = function (app) {
 
-module.exports = function (router) {
+    app.post('/register', function (req, res) {
+        var fb_info = req.body,
+        token = fb_info.authToken;
+        //modelPromise = model.setFbId(req.body.userid);
 
-    var model = new RegisterModel();
+        // console.log(fb_info);
 
+        // console.log(checkAccessToken(token));
 
-    router.get('/', function (req, res) {
-        
-        res.format({
-            json: function () {
-                res.json(model);
-            },
-            html: function () {
-                res.render('register/index', model);
-            }
-        });
+res.header("Content-Type", "application/json");
+model.checkIfRegistered(req.body.userid).then(function (result) {
+
+    console.log("IS USER IN DB?", result);
+
+    if(result === true) {
+            res.render('register', {'message': 
+                "{'userAlreadyRegistered' : 'true', 'status' : 'You are ALREADY registered, going to sign in page...'}"
+                });
+    }else {
+            model.setFbId(req.body.userid).then(function() {
+            res.render('register', 
+                {'message': "{'userAlreadyRegistered' : 'false', 'status' : 'Awesome, you are now registered!'}"
+            });
+        }).fail(function(err) {
+                console.log(err);
+            });            
+        }
     });
 
+});
+
+    var checkAccessToken = function(token) {
+        var t = app.get('https://graph.facebook.com/me?fields=id&access_token=' + token);
+        console.log("VALID?", t);
+        if(t.id)
+            return true;
+        else 
+            return false;
+    };
+    
 };
--- models/register.js
+++ models/register.js
@@ -1,8 +1,87 @@
 'use strict';
+var promise = require('q'),
+    db = require('../lib/db.js');
 
+db.connect();
 
 module.exports = function RegisterModel() {
-    return {
-        name: 'register'
+    this.setFbId = function(fb_id) {
+        var collectionPromise = promise.defer();
+        var sql = 'INSERT INTO users (`facebook-id`) VALUES (' + fb_id + ')';
+        db.query(sql, function(rows) {
+                        console.log(rows);
+                        collectionPromise.resolve(rows);
+                    });
+                return collectionPromise.promise;
+
     };
+
+    this.checkIfRegistered = function(fb_id) {
+
+      var collectionPromise = promise.defer();
+        var sql = 'SELECT `facebook-id` FROM users WHERE `facebook-id` =' + fb_id;
+        db.query(sql, function(rows) {
+                    
+                    if(rows.length > 0)
+                        collectionPromise.resolve(true);
+                    else
+                        collectionPromise.resolve(false);
+
+                    });
+              console.log(collectionPromise.promise);
+              return collectionPromise.promise;
+    };
+
+    // this.getUserName = function(userId) {
+    //     var sql = 'SELECT username FROM users WHERE userid=' + userId;
+    //       var collectionPromise = promise.defer();
+
+    //     this.dbQuery(sql, collectionPromise);
+    //     return collectionPromise.promise;
+
+    // };
+
+
+    // this.getUserCollectionInfo = function(userId){
+    //     var sql = 'SELECT * from user_collection WHERE userid = ' + userId;
+    //     var collectionPromise = promise.defer();
+
+    //     this.dbQuery(sql, collectionPromise);
+
+    // };
+
+    // this.setUserCollectionInfo = function(insertId, userId){
+    //       var sql = 'INSERT INTO user_collection (userId, collectionId) VALUES (' + userId + ',' + insertId + ')';
+    //     var collectionPromise = promise.defer();
+
+    //     this.dbQuery(sql, collectionPromise);
+
+    // };
+
+    // this.createCollectionForUser = function(name, userId, description, visibility){
+    //     var sql = 'INSERT INTO collections (name, description, visibility) VALUES (' + name + ',' + description + ',' + visibility + ')';
+
+    //     var collectionPromise = promise.defer(), self = this;
+
+    //    this.dbQuery(sql, collectionPromise, function(t){
+    //         self.setUserCollectionInfo(t, userId);
+    //    });
+
+     //  this.getUserCollectionInfo(
+
+        //var getNewCollection = 'SELECT collectionId from 
+
+    //};
+
+       this.dbQuery = function(sql, promise, callback) {
+
+        db.query(sql, function(rows) {
+            // if(rows.insertId !== undefined){
+            //     callback(rows.insertId);
+            // }
+            callback(rows);
+            promise.resolve(rows);
+        });
+    };
+
 };
