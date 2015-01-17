28dc9ebc8900ce057d6b3d54d69cfdff48ffa01c
pcanella <pcanella@ebay.com>
2015-01-16 23:44:15 -0800

    Added various updates

--- controllers/myCar/index.js
+++ controllers/myCar/index.js
@@ -7,59 +7,104 @@ var glob = require('glob');
 var memcache = require('../../lib/cache');
 var passport = require('passport');
 
-module.exports = function (router) {
+module.exports = function(router) {
 
     var model = new myCarModel();
 
-    router.get('/myCar/:id', function (req, res, done) {
+    router.get('/myCar/:id', function(req, res) {
 
-       var userFbId = (req.session.passport && req.session.passport.user && req.session.passport.user.userid) ? req.session.passport.user.userid : '';
+        var userFbId = (req.session.passport && req.session.passport.user && req.session.passport.user.userid) ? req.session.passport.user.userid : '';
 
 
-      // IF THIS USER IS LOGGED IN
-      var idAsInt = parseInt(req.route.params.id, 10), // just parses the :id
-      baseUrl = req.protocol + '://' + req.get('host');
-          serveImages(idAsInt, baseUrl, function(imageFiles) {
-          model.userImages = imageFiles;
+        // IF THIS USER IS LOGGED IN
+        var idAsInt = parseInt(req.route.params.id, 10), // just parses the :id
+            baseUrl = req.protocol + '://' + req.get('host');
+        serveImages(userFbId, baseUrl, function(imageFiles) {
+            model.userImages = imageFiles;
         });
 
-      model.userPageExists(idAsInt).then(function(result) {
-          model.getUserIdFromFacebookId(userFbId).then(function(actualUserId) {
-            if (actualUserId !== '' || actualUserId !== null){
-              var uID = (actualUserId) ? actualUserId[0].user_id : '';
-              req.session.passport.user.pageId = uID;
-          }else{
-            var uID = '';
-          }
-          if (uID === idAsInt) {
-            model.userEdit = 'true';
-            //console.log("model", model);
-            res.render('myCar/index', model); 
-         }else if (result === true) {
-            model.userEdit = 'false';
-            //console.log("model", model);
-            res.render('myCar/index', model); 
-        }else {
-            res.render('errors/404', model);
-        }
-          })
+        var waterfall = require('async-waterfall');
+        var obj = {};
 
-      }).then(function(done){
-        done();
-      });    
+        waterfall([
+
+            function(callback) {
+                obj.userPageExists = model.userPageExists(idAsInt);
+                callback(null, obj);
+            },
+            function(arg1, callback) {
+                obj.userIdFromFacebookId = model.getUserIdFromFacebookId(userFbId);
+                callback(null, obj);
+            },
+            function(arg1, callback) {
+                obj.pageId = model.getUserNameFromPageId(idAsInt);
+                // arg1 now equals 'three'
+                callback(null, obj);
+            }
+        ], function(err, result) {
+            console.log(obj);
+            if (obj.userIdFromFacebookId !== '' || obj.userIdFromFacebookId !== null) {
+                var uID = (obj.userIdFromFacebookId) ? obj.userIdFromFacebookId[0].user_id : '';
+                req.session.passport.user.pageId = uID;
+            } else {
+                var uID = '';
+            }
+            if (uID === idAsInt) {
+                model.userEdit = 'true';
+                model.
+                //console.log("model", model);
+                res.render('myCar/index', model);
+            } else if (obj.userPageExists === true) {
+                model.userEdit = 'false';
+                //console.log("model", model);
+                res.render('myCar/index', model);
+            } else {
+                res.render('errors/404', model);
+            }
+            // result now equals 'done'
+        });
+
+        //model.userPageExists(idAsInt).then(function(result) {
+        //model.getUserIdFromFacebookId(userFbId).then(function(actualUserId) {
+        // if (actualUserId !== '' || actualUserId !== null) {
+        //     var uID = (actualUserId) ? actualUserId[0].user_id : '';
+        //     req.session.passport.user.pageId = uID;
+        // } else {
+        //     var uID = '';
+        // }
+        // if (uID === idAsInt) {
+        //     model.userEdit = 'true';
+        //     model.
+        //     //console.log("model", model);
+        //     res.render('myCar/index', model);
+        // } else if (result === true) {
+        //     model.userEdit = 'false';
+        //     //console.log("model", model);
+        //     res.render('myCar/index', model);
+        // } else {
+        //     res.render('errors/404', model);
+        // }
+        //})
+
+        //        })
+        // .then(function(done) {
+        //     done();
+        // });
     });
 
     router.post('/myCar', function(req, res) {
         //console.log(t.userid);
         var fs = require('fs');
-         var form = new formidable.IncomingForm();
-         console.log(req.session.passport.user.pageId);
+        var form = new formidable.IncomingForm();
+        console.log(req.session.passport.user.pageId);
         var userIdImageFolder = path.resolve('public/images/' + req.session.passport.user.userid);
 
+        console.log(userIdImageFolder);
+
         fs.mkdir(userIdImageFolder, function(err) {
-          console.log(err);
+            console.log(err);
         });
-           
+
         form.uploadDir = userIdImageFolder;
         form.keepExtensions = true;
 
@@ -72,25 +117,27 @@ module.exports = function (router) {
 
         });
 
-         res.render('myCar/index', model);
+        res.render('myCar/index', model);
     });
 
 
 
     function serveImages(userId, baseUrl, fn) {
-      var images = [],
-      t = path.resolve('public/images/' + userId);
-      glob(t + "/*", function (er, files) {
+        var images = [],
+            t = path.resolve('public/images/' + userId);
+        glob(t + "/*", function(er, files) {
 
-        files.forEach(function(file) {
-          var newURI = file.split('public');
-          newURI = baseUrl + newURI[1];
-          images.push({'image': newURI});
-        });  
-          return fn(images);
-      });
+            files.forEach(function(file) {
+                var newURI = file.split('public');
+                newURI = baseUrl + newURI[1];
+                images.push({
+                    'image': newURI
+                });
+            });
+            return fn(images);
+        });
 
-      
+
 
 
     }
--- models/myCar.js
+++ models/myCar.js
@@ -9,9 +9,9 @@ module.exports = function myCarModel() {
         var promise = promise.defer();
         var sql = 'INSERT INTO user_cars (user_id, car_id) VALUES(' + user_id + ',' + car_id + ');';
         db.query(sql, function(rows) {
-                        promise.resolve(rows);
-                    });
-                return promise.promise;
+            promise.resolve(rows);
+        });
+        return promise.promise;
 
     };
 
@@ -21,10 +21,30 @@ module.exports = function myCarModel() {
         var promise = promise.defer();
         var sql = 'INSERT INTO user_cars (user_id, car_id) VALUES(' + user_id + ',' + car_id + ');';
         db.query(sql, function(rows) {
-                        promise.resolve(rows);
-                    });
-                return promise.promise;
+            promise.resolve(rows);
+        });
+        return promise.promise;
     };
+
+    this.getUserNameFromPageId = function(page_id) {
+        var aPromise = promise.defer();
+        var sql = 'SELECT `facebook-firstname`, `facebook-lastname` FROM users WHERE `user_id`=' + page_id;
+        db.query(sql, function(rows) {
+            aPromise.resolve(rows);
+        });
+        return aPromise.promise;
+
+    }
+
+    this.getUserNameFromFacebookId = function(fb_id) {
+        var unamePromise = promise.defer();
+        var sql = 'SELECT `facebook-firstname`, `facebook-lastname` FROM users WHERE `facebook-id`=' + fb_id;
+        db.query(sql, function(rows) {
+            unamePromise.resolve(rows);
+        });
+        return unamePromise.promise;
+
+    }
 
     // TODO: Add a check - if they have other pics, don't make profile_image true, else make profile image true
     this.setUserImagePath = function(fb_id, user_id, filepath) {
@@ -37,43 +57,43 @@ module.exports = function myCarModel() {
     };
 
     this.getUserIdFromFacebookId = function(facebook_id) {
-            console.log('getUserIdFromFacebookId');
-            var fbPromise = promise.defer();
-            if (facebook_id !== '') {
-                var sql = 'SELECT user_id from users WHERE `facebook-id` =' + facebook_id;
-                db.query(sql, function(rows) {
-                if(typeof rows === 'undefined') {
+        console.log('getUserIdFromFacebookId');
+        var fbPromise = promise.defer();
+        if (facebook_id !== '') {
+            var sql = 'SELECT user_id from users WHERE `facebook-id` =' + facebook_id;
+            db.query(sql, function(rows) {
+                if (typeof rows === 'undefined') {
                     console.log(rows, 'crap!!!, undefined');
-                   fbPromise.resolve(false);
-                }else{
+                    fbPromise.resolve(false);
+                } else {
                     fbPromise.resolve(rows);
                 }
-                });
-            }else{
-                fbPromise.resolve(false);
-            }
+            });
+        } else {
+            fbPromise.resolve(false);
+        }
         return fbPromise.promise;
     };
 
-    this.userPageExists = function(id){
+    this.userPageExists = function(id) {
         console.log('userpageexists');
         var userPagePromise = promise.defer();
         var sql = 'SELECT user_id from users WHERE `user_id` =' + id;
         db.query(sql, function(rows) {
-             if(typeof rows === 'undefined') {
+            if (typeof rows === 'undefined') {
                 console.log('ugh');
                 userPagePromise.resolve(false);
             };
-            if (rows.length > 0){
+            if (rows.length > 0) {
                 userPagePromise.resolve(true);
-            }else{
+            } else {
                 userPagePromise.resolve(false);
             }
         });
         return userPagePromise.promise;
     };
 
-       this.dbQuery = function(sql, promise, callback) {
+    this.dbQuery = function(sql, promise, callback) {
 
         db.query(sql, function(rows) {
             callback(rows);
